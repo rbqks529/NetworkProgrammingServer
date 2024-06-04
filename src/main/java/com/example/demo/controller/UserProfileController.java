25676df6682cde2cd47b01d000fcf0548b538305
JoGyuBin <rbqls3529@gmail.com>
2024-06-04 14:50:36 +0900

    초기

--- src/main/java/com/example/demo/controller/UserProfileController.java
+++ src/main/java/com/example/demo/controller/UserProfileController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.UserProfile;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,9 +23,33 @@ public class UserProfileController {
         userMap.put("3", new UserProfile("3", "홍길순", "111-1113", "서울시 강남구 대치3동"));
     }
 
-    @GetMapping("uer/{id}")
+    @GetMapping("/user/{id}")
     public UserProfile getUser(@PathVariable("id") String id) {
         return userMap.get(id);
+    }
+
+    @GetMapping("/user/all")
+    public List<UserProfile> getUserProfileList() {
+        return new ArrayList<UserProfile>(userMap.values());
+    }
+
+    @PutMapping("/user/{id}")
+    public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+        UserProfile userProfile = new UserProfile(id, name, phone, address);
+        userMap.put(id, userProfile);
+    }
+
+    @PostMapping("/user/{id}")
+    public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+        UserProfile userProfile = userMap.get(id);
+        userProfile.setName(name);
+        userProfile.setPhone(phone);
+        userProfile.setAddress(address);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUserProfile(@PathVariable("id") String id) {
+        userMap.remove(id);
     }
 
 }
